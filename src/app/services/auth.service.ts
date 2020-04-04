6a37f582d90b5f9930aa1fe70ecad4ab297503c2
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-04-04 17:33:07 -0300

    Initial deployment - login/logout methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .map(response => {
+        let result = response.json()
+        if (result && result.token) {
+          localStorage.setItem('token', result.token)
+          return true
+        }
+        return false
+      });
   }
 
-  logout() { 
+  logout() {
+    localStorage.removeItem('token')
   }
 
-  isLoggedIn() { 
-    return false;
+  isLoggedIn() {
+
+    //alternate method to replace all the code below
+    // with a pre-build jwt2 library:
+    return tokenNotExpired();
+
+    //CODE REPLACED:
+
+    /*let jwtHelper = new JwtHelper()
+    let token = localStorage.getItem('token')
+    if (!token) {
+      return false
+    }
+
+    let expirationDate = jwtHelper.getTokenExpirationDate(token)
+    let isExpired = jwtHelper.isTokenExpired(token)
+
+
+    return !isExpired;*/
   }
 }
 
